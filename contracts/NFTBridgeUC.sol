85a87f09775757dd02456c6e76040bcbe02d7bcc
brucelee0110 <bruce111>
2024-03-24 12:28:43 -0700

    add testing function for packet Received

--- contracts/NFTBridgeUC.sol
+++ contracts/NFTBridgeUC.sol
@@ -6,12 +6,10 @@ import "./base/UniversalChanIbcApp.sol";
 import "./interfaces/NonFungibleTokenPacketData.sol";
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
- import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 import "forge-std/console.sol";
-
-
 
 contract NFTBridgeUC is UniversalChanIbcApp, ERC721, IERC721Receiver {
     // application specific state
@@ -19,299 +17,311 @@ contract NFTBridgeUC is UniversalChanIbcApp, ERC721, IERC721Receiver {
     address private maintainer;
 
     mapping(uint64 => address) private destPortAddr;
-    mapping(uint64 => bytes32) private channelId; 
+    mapping(uint64 => bytes32) private channelId;
 
-
-    constructor(address _middleware) UniversalChanIbcApp(_middleware) ERC721("PolymerBridgeVoucher", "PBV") {
-      maintainer = msg.sender;
+    constructor(
+        address _middleware
+    ) UniversalChanIbcApp(_middleware) ERC721("PolymerBridgeVoucher", "PBV") {
+        maintainer = msg.sender;
     }
 
-    
-
-    // APPLICATION SPECIFIC LOGIC 
+    // APPLICATION SPECIFIC LOGIC
 
     function ping() public pure returns (string memory ret) {
-      ret = "pong";
+        ret = "pong";
     }
 
-    function configureBridge(uint64 _chain, address _dstPort, bytes32 _channelId) public onlyOwner {
-      destPortAddr[_chain] = _dstPort;
-      channelId[_chain] = _channelId;
+    function configureBridge(
+        uint64 _chain,
+        address _dstPort,
+        bytes32 _channelId
+    ) public onlyOwner {
+        destPortAddr[_chain] = _dstPort;
+        channelId[_chain] = _channelId;
     }
 
-    function initiateSend(address _nftContract, uint256 _tokenId, address _receiver, uint64 _chainId) external {
+    function initiateSend(
+        address _nftContract,
+        uint256 _tokenId,
+        address _receiver,
+        uint64 _chainId
+    ) external {
+        IERC721Metadata nft = IERC721Metadata(_nftContract);
 
-      IERC721Metadata nft = IERC721Metadata(_nftContract);
-      
-      NonFungibleTokenPacketData memory nftpd = NonFungibleTokenPacketData({
-          classId: _nftContract,
-          classUri: nft.name(),
-          classData: "", 
-          hops: "", 
-          tokenId: _tokenId,
-          tokenUri: nft.tokenURI(_tokenId),
-          tokenData: "",
-          sender: msg.sender,
-          receiver: _receiver, 
-          memo: ""
+        NonFungibleTokenPacketData memory nftpd = NonFungibleTokenPacketData({
+            classId: _nftContract,
+            classUri: nft.name(),
+            classData: "",
+            hops: "",
+            tokenId: _tokenId,
+            tokenUri: nft.tokenURI(_tokenId),
+            tokenData: "",
+            sender: msg.sender,
+            receiver: _receiver,
+            memo: ""
         });
 
-      if (isNFTOwner(_nftContract, _tokenId, msg.sender)) { // real
-     
-        _sendUniversalPacket(nftpd, _chainId);
+        if (isNFTOwner(_nftContract, _tokenId, msg.sender)) {
+            // real
 
+            _sendUniversalPacket(nftpd, _chainId);
+        } else if (isNFTOwner(address(this), _tokenId, msg.sender)) {
+            // voucher
 
-      } else if (isNFTOwner(address(this), _tokenId, msg.sender)) { // voucher
-      
-        _sendUniversalPacket(nftpd, _chainId);
-      } else {
-        revert("Caller does not own specified NFT");
-      }
+            _sendUniversalPacket(nftpd, _chainId);
+        } else {
+            revert("Caller does not own specified NFT");
+        }
     }
 
-    function isNFTOwner(address _nftContract, uint256 _tokenId, address _owner) private view returns (bool) {
-      return IERC721(_nftContract).ownerOf(_tokenId) == _owner;
+    function isNFTOwner(
+        address _nftContract,
+        uint256 _tokenId,
+        address _owner
+    ) private view returns (bool) {
+        return IERC721(_nftContract).ownerOf(_tokenId) == _owner;
     }
 
+    //  bytes memory dataURI = abi.encodePacked(
+    //       '{',
+    //           '"name": "My721Token #', tokenId.toString(), '"',
+    //           // Replace with extra ERC721 Metadata properties
+    //       '}'
+    //   );
 
+    //  return string(
+    //       abi.encodePacked(
+    //           "data:application/json;base64,",
+    //           Base64.encode(dataURI)
+    //       )
+    //   );
 
-  
-    
-
-  
-
-      //  bytes memory dataURI = abi.encodePacked(
-      //       '{',
-      //           '"name": "My721Token #', tokenId.toString(), '"',
-      //           // Replace with extra ERC721 Metadata properties
-      //       '}'
-      //   );
-
-      //  return string(
-      //       abi.encodePacked(
-      //           "data:application/json;base64,",
-      //           Base64.encode(dataURI)
-      //       )
-      //   );
-  
-    
-
-    
-
-
-    function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data) external pure returns (bytes4) {
-
-      return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
-
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes memory data
+    ) external pure returns (bytes4) {
+        return
+            bytes4(
+                keccak256("onERC721Received(address,address,uint256,bytes)")
+            );
     }
 
-    
-    function _receive(bytes32 srcPort, NonFungibleTokenPacketData memory nftpd) internal {
-      // string prefix = srcPort + '/' + nftpd.
-      // prefix = data.sourcePort + '/' + data.sourceChannel
+    function _receive(
+        bytes32 srcPort,
+        NonFungibleTokenPacketData memory nftpd
+    ) internal {
+        // string prefix = srcPort + '/' + nftpd.
+        // prefix = data.sourcePort + '/' + data.sourceChannel
 
-      if (nftpd.hops) { 
-        // createUpdateClass;
-        _mint(nftpd.reciever, 2); 
+        if (nftpd.hops) {
+            // createUpdateClass;
+            _mint(nftpd.reciever, 2);
+        } else {
+            IERC721(nftpd.classId).transferFrom(
+                address(this),
+                nftpd.receiver,
+                nftpd.tokenId
+            );
+        }
 
-      } else {
-        IERC721(nftpd.classId).transferFrom(address(this), nftpd.receiver, nftpd.tokenId);
-      }
-
-  //   // we are source chain if classId is prefixed with packet's sourcePort and sourceChannel
-  //   source = data.classId.slice(0, len(prefix)) === prefix
-  //   for (var i in data.tokenIds) {
-  //     if source { // we are source chain, un-escrow token to receiver
-  //       nft.Transfer(data.classId.slice(len(prefix)), data.tokenIds[i], data.receiver, data.tokenData[i])
-  //     } else { // we are sink chain, mint voucher to receiver
-  //       prefixedClassId = data.destPort + '/' + data.destChannel + '/' + data.classId
-  //       nft.CreateOrUpdateClass(prefixedClassId, data.classUri, data.classData)
-  //       nft.Mint(prefixedClassId, data.tokenIds[i], data.tokenUris[i], data.tokenData[i], data.receiver)
-  //     }
-  //   }
-
+        //   // we are source chain if classId is prefixed with packet's sourcePort and sourceChannel
+        //   source = data.classId.slice(0, len(prefix)) === prefix
+        //   for (var i in data.tokenIds) {
+        //     if source { // we are source chain, un-escrow token to receiver
+        //       nft.Transfer(data.classId.slice(len(prefix)), data.tokenIds[i], data.receiver, data.tokenData[i])
+        //     } else { // we are sink chain, mint voucher to receiver
+        //       prefixedClassId = data.destPort + '/' + data.destChannel + '/' + data.classId
+        //       nft.CreateOrUpdateClass(prefixedClassId, data.classUri, data.classData)
+        //       nft.Mint(prefixedClassId, data.tokenIds[i], data.tokenUris[i], data.tokenData[i], data.receiver)
+        //     }
+        //   }
     }
 
-  // SUB-PROTOCOLS 
-
-
+    // SUB-PROTOCOLS
 
     // this needs to update hops before / after sending
-    function CreateOrUpdateClass() public {
+    function CreateOrUpdateClass() public {}
 
-    }
+    function Mint() public {}
 
-
-    
-    function Mint() public {
-
-
-    }
-
-
-    function Burn() public {
-
-
-
-    }
+    function Burn() public {}
 
     function GetOwner(uint256 tknid) public pure {
-      return ownerOf(tknid);
-
+        return ownerOf(tknid);
     }
 
-    // 
-    function getNFT() public {
+    //
+    function getNFT() public {}
 
-
-    }
-
-    function getClass() public {
-
-    }
+    function getClass() public {}
 
     function refundToken(UniversalPacket calldata packet) private {
-      IERC721(nftpd.classId).transferFrom(address(this), nftpd.receiver, nftpd.tokenId);
-      // _burn()
-      // _burn(1); // this may revert if not exists. 
+        IERC721(nftpd.classId).transferFrom(
+            address(this),
+            nftpd.receiver,
+            nftpd.tokenId
+        );
+        // _burn()
+        // _burn(1); // this may revert if not exists.
     }
 
+    // PACKET RELAY
 
-  // PACKET RELAY
+    //   function refundToken(packet: Packet) {
+    //     NonFungibleTokenPacketData data = packet.data
+    //     prefix = data.sourcePort + '/' + data.sourceChannel
+    //   // we are the source if the classId is not prefixed with the packet's sourcePort and sourceChannel
+    //     source = data.classId.slice(0, len(prefix)) !== prefix
+    //     for (var i in data.tokenIds) {
+    //       if source { // we are source chain, un-escrow token back to sender
+    //         nft.Transfer(data.classId, data.tokenIds[i], data.sender, null)
+    //       } else { // we are sink chain, mint voucher back to sender
+    //         nft.Mint(data.classId, data.tokenIds[i], data.tokenUris[i], data.tokenData[i], data.sender)
+    //       }
+    //     }
+    //   }
 
-//   function refundToken(packet: Packet) {
-//     NonFungibleTokenPacketData data = packet.data
-//     prefix = data.sourcePort + '/' + data.sourceChannel
-//   // we are the source if the classId is not prefixed with the packet's sourcePort and sourceChannel
-//     source = data.classId.slice(0, len(prefix)) !== prefix
-//     for (var i in data.tokenIds) {
-//       if source { // we are source chain, un-escrow token back to sender
-//         nft.Transfer(data.classId, data.tokenIds[i], data.sender, null)
-//       } else { // we are sink chain, mint voucher back to sender
-//         nft.Mint(data.classId, data.tokenIds[i], data.tokenUris[i], data.tokenData[i], data.sender)
-//       }
-//     }
-//   }
+    // Local Testing Functions
 
+    function onRecvPacket(
+        bytes32 _channelId,
+        UniversalPacket calldata packet
+    ) external returns (AckPacket memory ackPacket) {
+        // recvedPackets.push(UcPacketWithChannel(channelId, packet));
 
-    
+        NonFungibleTokenPacketData memory nftpd = abi.decode(
+            packet.appData,
+            (NonFungibleTokenPacketData)
+        );
 
+        _receive(packet.srcPortAddr, nftpd);
 
-//     // IBC logic
-
-  function _sendUniversalPacket(
-    NonFungibleTokenPacketData memory nftpd, uint64 _chain
-  ) internal {
-
-    // string prefix = sourcePort + "/" + sourceChannel;
-    // bool source = classId.slice(0, len(prefix))!== prefix;
-
-    require(channelId[_chain] != bytes32(0x0), "chain not configured");
-
-    if (_compare(nftpd.hops, "")) {
-      IERC721(nftpd.classId).safeTransferFrom(msg.sender, address(this), nftpd.tokenId);
-    } else { 
-      _burn(nftpd.tokenId);
+        return AckPacket(true, abi.encode("Acknowledged"));
     }
-    // token = nft.GetNFT(classId, tokenId)
-      // increment();
-      // bytes memory payload = abi.encode(msg.sender, counter);
 
-    bytes memory payload = abi.encode(nftpd);
-    
-    uint64 timeoutTimestamp = uint64((block.timestamp + 3600000) * 1000000000);
-  
-    IbcUniversalPacketSender(mw).sendUniversalPacket(
-        channelId[_chain], IbcUtils.toBytes32(destPortAddr[_chain]), payload, timeoutTimestamp
-    );
+    //     // IBC logic
 
-  }
-  
+    function _sendUniversalPacket(
+        NonFungibleTokenPacketData memory nftpd,
+        uint64 _chain
+    ) internal {
+        // string prefix = sourcePort + "/" + sourceChannel;
+        // bool source = classId.slice(0, len(prefix))!== prefix;
 
-      /**
+        require(channelId[_chain] != bytes32(0x0), "chain not configured");
+
+        if (_compare(nftpd.hops, "")) {
+            IERC721(nftpd.classId).safeTransferFrom(
+                msg.sender,
+                address(this),
+                nftpd.tokenId
+            );
+        } else {
+            _burn(nftpd.tokenId);
+        }
+        // token = nft.GetNFT(classId, tokenId)
+        // increment();
+        // bytes memory payload = abi.encode(msg.sender, counter);
+
+        bytes memory payload = abi.encode(nftpd);
+
+        uint64 timeoutTimestamp = uint64(
+            (block.timestamp + 3600000) * 1000000000
+        );
+
+        IbcUniversalPacketSender(mw).sendUniversalPacket(
+            channelId[_chain],
+            IbcUtils.toBytes32(destPortAddr[_chain]),
+            payload,
+            timeoutTimestamp
+        );
+    }
+
+    /**
      * @dev Packet lifecycle callback that implements packet receipt logic and returns and acknowledgement packet.
      *      MUST be overriden by the inheriting contract.
      *
      * @param _channelId the ID of the channel (locally) the packet was received on.
      * @param packet the Universal packet encoded by the source and relayed by the relayer.
      */
-    function onRecvUniversalPacket(bytes32 _channelId, UniversalPacket calldata packet)
-        external
-        override
-        onlyIbcMw
-        returns (AckPacket memory ackPacket)
-    {
-       // recvedPackets.push(UcPacketWithChannel(channelId, packet));
+    function onRecvUniversalPacket(
+        bytes32 _channelId,
+        UniversalPacket calldata packet
+    ) external override onlyIbcMw returns (AckPacket memory ackPacket) {
+        // recvedPackets.push(UcPacketWithChannel(channelId, packet));
 
-        NonFungibleTokenPacketData memory nftpd = abi.decode(packet.appData, (NonFungibleTokenPacketData));
-  
+        NonFungibleTokenPacketData memory nftpd = abi.decode(
+            packet.appData,
+            (NonFungibleTokenPacketData)
+        );
+
         _receive(packet.srcPortAddr, nftpd);
 
         return AckPacket(true, abi.encode("Acknowledged"));
     }
 
+    /**
+     * @dev Packet lifecycle callback that implements packet acknowledgment logic.
+     *      MUST be overriden by the inheriting contract.
+     *
+     * @param ack the acknowledgment packet encoded by the destination and relayed by the relayer.
+     */
+    function onUniversalAcknowledgement(
+        bytes32 _channelId,
+        UniversalPacket calldata packet,
+        AckPacket calldata ack
+    ) external override onlyIbcMw {
+        // ackPackets.push(ack);
 
-  /**
-    * @dev Packet lifecycle callback that implements packet acknowledgment logic.
-    *      MUST be overriden by the inheriting contract.
-    * 
-    * @param ack the acknowledgment packet encoded by the destination and relayed by the relayer.
-    */
-  function onUniversalAcknowledgement(bytes32 _channelId, UniversalPacket calldata packet, AckPacket calldata ack) external override onlyIbcMw {
-      // ackPackets.push(ack);
+        if (!ack.success) refundToken(packet);
 
+        // (uint64 _counter) = abi.decode(ack.data, (uint64));
 
-      if (!ack.success) refundToken(packet);
-      
-      // (uint64 _counter) = abi.decode(ack.data, (uint64));
-      
-      // if (_counter != counter) {
-      //   resetCounter();
-      // }
+        // if (_counter != counter) {
+        //   resetCounter();
+        // }
+    }
 
-  }
-
-
-
-
-
-// When a packet times out, tokens represented in the packet are either unescrowed or 
-// minted back to the sender appropriately -- 
-// depending on whether the tokens are being moved away from or back toward their source.
+    // When a packet times out, tokens represented in the packet are either unescrowed or
+    // minted back to the sender appropriately --
+    // depending on whether the tokens are being moved away from or back toward their source.
     /**
      * @dev Packet lifecycle callback that implements packet receipt logic and return and acknowledgement packet.
      *      MUST be overriden by the inheriting contract.
      *      NOT SUPPORTED YET
-     * 
+     *
      * @param _channelId the ID of the channel (locally) the timeout was submitted on.
      * @param packet the Universal packet encoded by the counterparty and relayed by the relayer
      */
-    function onTimeoutUniversalPacket(bytes32 _channelId, UniversalPacket calldata packet) external override onlyIbcMw {
+    function onTimeoutUniversalPacket(
+        bytes32 _channelId,
+        UniversalPacket calldata packet
+    ) external override onlyIbcMw {
         timeoutPackets.push(UcPacketWithChannel(_channelId, packet));
         // do logic
     }
 
-
-
     // HELPERS
 
-    function _compare(string memory str1, string memory str2) internal pure returns (bool) {
+    function _compare(
+        string memory str1,
+        string memory str2
+    ) internal pure returns (bool) {
         if (bytes(str1).length != bytes(str2).length) {
             return false;
         }
-        return keccak256(abi.encodePacked(str1)) == keccak256(abi.encodePacked(str2));
+        return
+            keccak256(abi.encodePacked(str1)) ==
+            keccak256(abi.encodePacked(str2));
     }
 
     function getChainId() public view returns (uint) {
-      return block.chainid;
-      
+        return block.chainid;
     }
 
-
-// function onTimeoutPacketClose(packet: Packet) {
-//   // can't happen, only unordered channels allowed
-// }
-
-
-
+    // function onTimeoutPacketClose(packet: Packet) {
+    //   // can't happen, only unordered channels allowed
+    // }
 }
